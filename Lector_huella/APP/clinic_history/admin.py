a77e09861a27c9d593ae65e7e67b26c77b3cad70
diegopar27 <colombiajuandiego@hotmail.com>
2022-10-17 13:44:08 -0500

    se modifico el modelo historia clinica se agrego RH enfermedad actual y motivo consulta y creacion de la base de datos

--- Lector_huella/APP/clinic_history/admin.py
+++ Lector_huella/APP/clinic_history/admin.py
@@ -5,6 +5,6 @@ from APP.clinic_history.models import History
 
 @admin.register(History)
 class HistoryAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Surname', 'Cc', 'Phone','gender', 'Direction',)
+    list_display = ('Name', 'Surname', 'Rh','Cc', 'Phone','gender', 'Direction',)
     list_editable = ('Phone','gender',)
     search_fields = ('Name', 'Cc',)
