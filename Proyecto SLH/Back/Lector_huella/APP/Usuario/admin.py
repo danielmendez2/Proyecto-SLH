5a5022d4564ed210d7875453446f205faf7950d0
diegopar27 <102553545+diegopar27@users.noreply.github.com>
2022-10-17 11:00:03 -0500

    Delete Proyecto SLH/Back directory
    para subir back nuevo

--- Proyecto SLH/Back/Lector_huella/APP/Usuario/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from APP.Usuario.models import Paciente
-# Register your models here.
-
-@admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('Name', 'Surname', 'Cc', 'age', 'Phone', 'gender', 'Direction',)
-    list_editable = ('Phone', 'gender',)
-    search_fields = ('Name', 'Cc',)
--- Proyecto SLH/Back/Lector_huella/APP/user/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from APP.user.models import User
-# Register your models here.
-
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('name', 'password', 'email')
-    search_fields = ('email',)
