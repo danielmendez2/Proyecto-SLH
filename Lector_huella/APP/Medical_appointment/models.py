8ddf4b43285c716a04b6a139e3f620e603527673
diegopar27 <colombiajuandiego@hotmail.com>
2022-10-23 21:17:44 -0500

    MODIFICACION DE LOS MODELOS DE CITAS Y PACIENTE

--- Lector_huella/APP/Medical_appointment/models.py
+++ Lector_huella/APP/Medical_appointment/models.py
@@ -8,10 +8,16 @@ from APP.Patient.models import Patients
 
 class Quotes(models.Model):
     date_and_time = models.DateTimeField()
-    doctors = models.ForeignKey(Doctors, on_delete=models.CASCADE)
-    patient = models.ForeignKey(Patients, on_delete=models.CASCADE)
-    eps = models.CharField(max_length=50)
-    specialty = models.CharField(max_length=50)
+    doctors = models.ForeignKey(Doctors, on_delete=models.CASCADE, blank=True, null=True,
+                                verbose_name='Doctor')
+    patient = models.ForeignKey(Patients, on_delete=models.CASCADE, blank=True, null=True,
+                                verbose_name='Paciente')
+    specialitys = [
+        ('C Y D','Crecimiento y desarrollo'),
+        ('V','Vacunacion'),
+    ]
+    specialty = models.CharField(max_length=50,choices=specialitys, blank=True, null=True,
+                                 verbose_name='consulta')
 
     def __str__(self):
         return self.patient.Name
--- Lector_huella/APP/Patient/models.py
+++ Lector_huella/APP/Patient/models.py
@@ -15,8 +15,21 @@ class Patients(models.Model):
         ('M', 'MASCULINO'),
         ('O', 'OTRO')
     ]
-    gender = models.CharField(max_length=1, choices = genders, verbose_name='Genero')
-    Eps = models.CharField(max_length=100, verbose_name='Eps')
+    gender = models.CharField(max_length=1, choices=genders, verbose_name='Genero')
+    EPSS = [
+        ('S','SANITAS'),
+        ('N', 'NUEVA EPS'),
+        ('F', 'FAMISANAR'),
+        ('CP', 'CAPITAL SALUD'),
+        ('A', 'ALIANSALUD'),
+        ('C', 'COMPENSAR'),
+        ('CO', 'COMMEVAEPS'),
+        ('SA', 'SALUD TOTAL'),
+        ('SU', 'SURA'),
+        ('SO', 'SOS'),
+        ('FU', 'FUNDACION SALUD MIA'),
+    ]
+    Eps = models.CharField(max_length=50, choices=EPSS, verbose_name='Eps')
     email = models.EmailField(max_length=100, verbose_name='Correo')
     date_of_birth = models.DateField(null=True, blank=True, verbose_name='Fecha de cumpleaños')
 
